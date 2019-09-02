99f312e90e940c05446140b99b852cc318c4897a
unknown <jean.miraval@utec.edu.pe>
2019-09-02 16:56:49 -0500

    Finished, en el radix, copiéde internet la parte del sort después de partir el array pq era simplemenete implementar otro tipo de sort dentro.

--- merge.h
+++ merge.h
@@ -7,8 +7,26 @@ class MergeSort : public Sort {
     public:
         MergeSort(int *elements, size_t size) : Sort(elements, size) {}
 
+    void mergue(int from,int to){
+        for(int i=from;i<=to;i++)
+            swap(*min_element(elements+i,elements+to+1),elements[i]);
+
+    }
+    void merguesort(int min_limit,int max_limit){
+        int mid;
+        if(min_limit<max_limit) {
+            mid = (min_limit + max_limit) / 2;
+            merguesort(min_limit, mid);
+            merguesort(mid + 1, max_limit);
+
+            mergue(min_limit, max_limit);
+        }
+
+    }
+
         void execute() {
             // TODO
+            merguesort(0, size);
         }
 
         inline string name() { return "MergeSort"; }
--- radix.h
+++ radix.h
@@ -7,8 +7,42 @@ class RadixSort : public Sort {
     public:
         RadixSort(int *elements, size_t size) : Sort(elements, size) {}
 
-        void execute() {
+    int getMax(int lista[], int size)
+    {
+        int mx = lista[0];
+        for (int i = 1; i < size; i++)
+            if (lista[i] > mx)
+                mx = lista[i];
+        return mx;
+    }
+
+    void countingSort(int arr[], int n, int exp)
+    {
+        int output[n];
+        int i, count[10] = {0};
+        for (i = 0; i < n; i++)
+            count[ (arr[i]/exp)%10 ]++;
+        for (i = 1; i < 10; i++)
+            count[i] += count[i - 1];
+
+        // Build the output array
+        for (i = n - 1; i >= 0; i--)
+        {
+            output[count[ (arr[i]/exp)%10 ] - 1] = arr[i];
+            count[ (arr[i]/exp)%10 ]--;
+        }
+
+        // Copy the output array to arr[], so that arr[] now
+        // contains sorted numbers according to current digit
+        for (i = 0; i < n; i++)
+            arr[i] = output[i];
+    }
+
+    void execute() {
             // TODO
+            int x = getMax(elements, size);
+            for (int exp = 1; x/exp > 0; exp *= 10)
+                countingSort(elements, size, exp);
         }
 
         inline string name() { return "RadixSort"; }
